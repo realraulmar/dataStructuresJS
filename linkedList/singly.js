a292225f0973506c587afceca9ca90adc53c6930
raulmar <raulhdz40@gmail.com>
2021-01-27 17:42:46 -0600

    Singly Linked List w/append, prepend, insert and remove

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -35,15 +35,61 @@ class MySinglyLinkedList {
     this.length = 1
   }
   append(value) {
-    let currentNode = new Node(value)
-    this.tail.next = currentNode
-    this.tail = currentNode
+    const newNode = new Node(value)
+    this.tail.next = newNode
+    this.tail = newNode
     this.length++
+    return this
+  }
+  prepend(value) {
+    const newHead = new Node(value)
+    newHead.next = this.head
+    this.head = newHead
 
+    this.length++
+  }
+  insert(index, value) {
+    if(index >= this.length) {
+      return this.append(value)
+    } else if(index <= 0) {
+      return this.prepend(value)
+    }
+
+    const newNode = new Node(value)
+    const firstPointer = this.getIndex(index - 1)
+    const holdingPointer = firstPointer.next
+    firstPointer.next = newNode
+    newNode.next = holdingPointer
+    this.length++
+    return this
+  }
+  getIndex(index) {
+    let counter = 0
+    let currentNode = this.head
+    while(counter !== index) {
+      currentNode = currentNode.next
+      counter++
+    }
+    return currentNode
+  }
+  remove(index) {
+    if(index >= this.length || index < 0) {
+      return console.error(`No existe el nodo ${index}`)
+    }
+    const beforeNodeToRemove = this.getIndex(index - 1)
+    const nodeToRemove = this.getIndex(index)
+    const holdingPointer = nodeToRemove.next
+    beforeNodeToRemove.next = holdingPointer
+    this.length--
+    return this
   }
 }
 
-let mySinglyLinkedList = new MySinglyLinkedList
+let mySinglyLinkedList = new MySinglyLinkedList(1)
 mySinglyLinkedList.append(2)
 mySinglyLinkedList.append(3)
+mySinglyLinkedList.append(4)
+mySinglyLinkedList.prepend(0)
+mySinglyLinkedList.insert(3,2.5)
+mySinglyLinkedList.remove(3)
 console.log(mySinglyLinkedList)
